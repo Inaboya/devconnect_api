95013f9809015afceb2d0a2f783291ca5f9f9281
Inaboya <momohdavid21@gmail.com>
2023-03-01 13:07:17 +0100

    build schema

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('DevConnect API')
+    .setDescription('The DevConnect API description')
+    .setVersion('1.0')
+    .addTag('devconnect')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { UsersService } from './users.service';
 
+@ApiTags('Users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create user' })
+  @ApiCreatedResponse({
+    status: 201,
+    description: 'The user has been successfully created.',
+    type: UserDTO,
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Bad request',
+  })
+  @Post()
+  async registerUser(@Body() payload: CreateUserDTO) {
+    return await this.userService.registerUser(payload);
+  }
+}

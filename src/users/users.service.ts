2e57013ca9861d084d21ffdd91fe3a555164e744
Inaboya <momohdavid21@gmail.com>
2023-03-02 13:11:45 +0100

    jwt service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,24 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { validateRegisterInput } from 'src/utils/validateAuth';
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from 'src/utils/validateAuth';
 import { CreateUserDTO } from './dto/create-users-dto';
 import { UserInterface } from './interface/user-interface';
 // import * as normalizeUrl from 'normalize-url';
 import * as gravatar from 'gravatar';
 import * as bcrypt from 'bcryptjs';
 import { normalizeUrl } from 'src/utils/normalize-url';
+import { LoginUserDTO } from './dto/login-users-dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel('User') private readonly userModel: Model<UserInterface>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async registerUser(payload: CreateUserDTO) {
@@ -60,6 +66,56 @@ export class UsersService {
         const createdUser = await newUser.save();
 
         return createdUser;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async loginUser(payload: LoginUserDTO) {
+    const errors = validateLoginInput(payload);
+    try {
+      if (errors.isValid === false) {
+        throw new HttpException(
+          { status: HttpStatus.BAD_REQUEST, errors },
+          HttpStatus.BAD_REQUEST,
+        );
+      } else {
+        const { email, password } = payload;
+
+        const user = await this.userModel.findOne({ email });
+
+        if (!user) {
+          throw new HttpException(
+            {
+              status: HttpStatus.BAD_REQUEST,
+              errors: 'Invalid credentials',
+            },
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+          throw new HttpException(
+            {
+              status: HttpStatus.BAD_REQUEST,
+              errors: 'Invalid credentials',
+            },
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+
+        const payload = {
+          id: user.id,
+        };
+
+        const token = this.jwtService.sign(payload, {
+          expiresIn: '5 days',
+        });
+
+        return { data: user, token };
       }
     } catch (error) {
       throw error;
--- src/utils/validateAuth.ts
+++ src/utils/validateAuth.ts
@@ -8,6 +8,11 @@ interface DataInput {
   password2: string;
 }
 
+interface LoginInput {
+  email: string;
+  password: string;
+}
+
 interface error {
   [key: string]: string;
 }
@@ -41,6 +46,27 @@ export const validateRegisterInput = (data: DataInput) => {
 
   if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = 'Passwords must match';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
+
+export const validateLoginInput = (data: LoginInput) => {
+  const errors: error = {};
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
   }
 
   return {

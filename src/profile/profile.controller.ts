a5e5d35cc49005544e5c6114a8dbe6b3dab37bb3
Inaboya <momohdavid21@gmail.com>
2023-03-03 17:46:05 +0100

    build profile controller

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Req } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { CreatePostDTO } from './dto/create-post-dto';
 
 @Controller('profile')
-export class ProfileController {}
+export class ProfileController {
+    constructor(private readonly postService: PostService) {}
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create a post' })
+  @ApiCreatedResponse({
+    status: 201,
+    description: 'Post created',
+  })
+  @ApiUnauthorizedResponse({
+    status: 401,
+    description: 'Unauthorized.',
+  })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Bad request',
+  })
+  createPost(@Body() payload: CreatePostDTO, @Req() req) {
+    return this.postService.createPost(payload, req.user);
+  }
+}

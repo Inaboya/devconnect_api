0c7bdbfc5241a1b52ca2d313db3cf996d37a579a
Inaboya <momohdavid21@gmail.com>
2023-03-13 17:36:00 +0100

    build validation method for education input

---
+++ src/utils/validateEducation.ts
@@ -0,0 +1,41 @@
+import Validator from 'validator';
+import { isEmpty } from './is-empty';
+
+interface EducationInput {
+  school: string;
+  degree: string;
+  fieldofstudy: string;
+  from: string;
+  to: string;
+  current: boolean;
+  description: string;
+}
+
+interface error {
+  [key: string]: string;
+}
+
+export const validateEducationInput = (data: EducationInput) => {
+  const errors: error = {};
+
+  if (Validator.isEmpty(data.school)) {
+    errors.school = 'School field is required';
+  }
+
+  if (Validator.isEmpty(data.degree)) {
+    errors.degree = 'Degree field is required';
+  }
+
+  if (Validator.isEmpty(data.fieldofstudy)) {
+    errors.fieldofstudy = 'Field of study field is required';
+  }
+
+  if (Validator.isEmpty(data.from)) {
+    errors.from = 'From date field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
